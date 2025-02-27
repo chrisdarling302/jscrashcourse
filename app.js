65c44e01cfc9e1bfa77f026a5e00a6cf18bf61f4
Chris <cldarling@gmail.com>
2025-02-27 17:22:04 -0500

    last nights work

--- app.js
+++ app.js
@@ -59,9 +59,62 @@ let loggedIn = true
 let str = subscribed || loggedIn ? 'show video' : 'hide video'
 console.log(str) */
 
-let cash = 30
+/** let cash = 30
 let price = 40
 let isStoreOpen = true
 
 let str = isStoreOpen && cash >= price ? "give receipt" : "do not give receipt"
 console.log(str)
+*/
+//let count = 1
+
+//while (count <=100) {
+ // count = count + 1;
+ // console.log(count)
+//}
+
+/**for (let i = 0; i < 100; i++) {
+  console.log(i +1)
+}
+  */
+
+/** write a for-loop that loops through 1 to 20
+ * if the number is divisible by 3, print "Frontend"
+ * if the number is divisible by 5, print "Simplified"
+ * If the number is difivisble by 3 and 5, print "Frontend Simplified"
+ * If the number is *not* divisible by either 3 or 5, print the number
+ * 
+ * example
+ * 1 -> 1
+ * 2 -> 2
+ * 3 -> "Frontend"
+ * 4 -> 4
+ * 5 -> "Simplified"
+ * ...
+ * 15 -> "Frontend Simplified"
+ * ...
+ * 20 -. "Simplified"
+ */
+
+/** for (let i = 1; i <= 20; ++i) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`)
+  }
+  else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`)
+  }
+  else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+  }
+  else {
+    console.log (`${i} -> ${i}`)
+  }
+  }
+  */
+
+  const str = "Frontend Simplified"
+  for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+  }
+
+"Frontend Simplified".length
