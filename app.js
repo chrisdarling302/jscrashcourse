01a9de65b2df59afd0cbfad59e39e9f168232d1e
Chris <cldarling@gmail.com>
2025-03-04 21:26:53 -0500

    finished Promises

--- app.js
+++ app.js
@@ -364,3 +364,58 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
   */
 
 // 2. Async/Await
+
+const emailRef = document.querySelector(".email") //this is a variable
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
+
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+  const data = await response.json() 
+  console.log(data)
+  emailRef.innerHTML = data.email
+}
+
+main();
+
+//How to create a promise:
+
+function getSubscriptionStatus() {
+ return new Promise((resolve, reject) => {
+    resolve(undefined)
+  })
+}
+// 1. Then
+//getSubscriptionStatus().then(response => console.log(response))
+
+// 2. Async/Await
+
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video")
+    }
+    else if (subscriptionStatus === "FREE") {
+      resolve("show trailer")
+    }
+    else {
+      reject("no video")
+    }
+  })
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+    console.log(await getVideo(status))
+  }
+  catch (e) {
+    console.log(e)
+    videoRef.innerHTML = e;
+  }
+
+}
+
+main()
