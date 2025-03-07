b9af30245d2cc824b20c894e2ca82febff208c3b
Chris <cldarling@gmail.com>
2025-03-06 21:57:51 -0500

    beginner challenges complete

--- Beginner Challenges/app.js
+++ Beginner Challenges/app.js
@@ -55,3 +55,68 @@ function lessThanOrEqualToZero(num) {
 console.log(lessThanOrEqualToZero(3));
 console.log(lessThanOrEqualToZero(0));
 console.log(lessThanOrEqualToZero(-2));
+
+//Opposite boolean
+function oppositeBoolean(bool) {
+  return !bool;
+}
+
+console.log(oppositeBoolean(true))
+console.log(oppositeBoolean(false))
+
+//Is not the number 0
+
+function isNotZero(elem) {
+  return elem !== 0;
+}
+
+console.log(isNotZero(5))
+console.log(isNotZero(0))
+console.log(isNotZero(null))
+
+//Calculate the remainder
+
+function calcRemainder(num1, num2) {
+  return num1 % num2;
+}
+
+console.log(calcRemainder(4, 2))
+console.log(calcRemainder(7, 8))
+console.log(calcRemainder(9, 8))
+
+//Is the number odd?
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(1))
+console.log(isOdd(10))
+
+//If a number is event, return 1 otherwise return -1 (using a Ternary Operator)
+function booleanInteger(num) {
+  return num % 2 === 0 ? 1 : -1;
+}
+
+console.log(booleanInteger(1));
+console.log(booleanInteger(2));
+console.log(booleanInteger(5));
+
+//Check if a user is logged in AND subscribed
+
+function isLoggedInAndSubscribed(loggedIn, subscribed) {
+  return (loggedIn === "LOGGED_IN") && (subscribed === "SUBSCRIBED")
+}
+
+console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"))
+console.log(isLoggedInAndSubscribed("LOGGED_IN", "UNSUBSCRIBED"))
+console.log(isLoggedInAndSubscribed("LOGGED_OUT", "SUBSCRIBED"))
+
+//Check if a user is logged in OR subscribed
+function isLoggedInAndSubscribed(loggedIn, subscribed) {
+  return (loggedIn === "LOGGED_IN") || (subscribed === "SUBSCRIBED")
+}
+
+console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"));
+console.log(isLoggedInAndSubscribed("LOGGED_OUT", "UNSUBSCRIBED"));
+console.log(isLoggedInAndSubscribed("LOGGED_OUT", "SUBSCRIBED"));
